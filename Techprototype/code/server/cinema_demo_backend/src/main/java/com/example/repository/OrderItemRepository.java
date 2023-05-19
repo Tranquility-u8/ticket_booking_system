5133a08eaaf2f32fba4decfcf7a2cced918e51c2
Zeng Yongcheng <127168960+SJTU-ZYC@users.noreply.github.com>
2023-05-19 18:08:34 +0800

    Delete Techprototype/code/server directory

--- Techprototype/code/server/cinema_demo_backend/src/main/java/com/example/repository/OrderItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.OrderItemEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItemEntity, Integer> {
-
-}
--- Techprototype/code/server/cinema_demo_backend/src/main/java/com/example/repository/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.TicketEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<TicketEntity, Integer> {
-}
